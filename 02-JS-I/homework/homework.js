5c577e8164044ef14932ea6abc0bdb841d44745d
segundodemiranda <demirandasegundo@gmail.com>
2022-02-05 16:16:51 -0300

    subiendohomework JSI

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -55,6 +55,8 @@ function divide(x, y) {
   // Tu código:
   return x / y
 
+}
+
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
@@ -66,36 +68,57 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1 == str2)
-}
+  if (str1.length =str2.length)
+   {return true}
+   {return false}
 
+}  
+ 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
-  // Tu código:
+  // Tu código: 
+  if(num < 90)
+  {return true}
+  else
+  {return false}
 }
 
 function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (num > 50)
+  {return true}
+  else
+  {return false}
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
+  return x % y
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+  
+  if((num % 2) = 0)
+  {return true}
+  else
+  {return false}
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+  if((num % 2)= 0)
+  {return false}
+  else
+  {return true}
 }
 
 function elevarAlCuadrado(num) {
@@ -227,4 +250,4 @@ module.exports = {
   areaDelTriangulo,
   deEuroAdolar,
   esVocal,
-};
+  }
